73d1c0131c565354dcac43edfecb12b94aeb7448
shohan12727 <aishohan001@gmail.com>
2026-07-06 00:59:20 +0600

    sum scope spaces tuple added

--- RS-2/src/main.rs
+++ RS-2/src/main.rs
@@ -5,10 +5,32 @@ fn sum () {
     println!("Total of two number is: {total}");
 }
 
+fn scope (){
+    let x = 5;
+    let x = x +1;
+    {
+        let x = x * 2;
+        println!("The value of of x is the inner scope is: {x}");
+    }
+    println!("The value of x is: {x}")
+}
 
+fn spaces () {
+    let spaces = "Brazil";
+    let spaces = spaces.len();
+    println!("{spaces}")
+}
+
+
+fn tuple(){
+  let tup: (i32, f64, u8) = (23, 4.654, 96);
+  println!("{:?}", tup)
+}
 
 
 fn main() {
-
-    sum()
+    // sum();
+    // scope();
+    // spaces();
+    // tuple();
 }
