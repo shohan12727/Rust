a76d56833fe0a13bc0df3ab3a70f8b70b79da807
shohan12727 <aishohan001@gmail.com>
2026-07-08 11:06:39 +0600

    refactor: reorganize functions and update main to call slicing

--- RS-3/src/main.rs
+++ RS-3/src/main.rs
@@ -12,11 +12,17 @@ fn reference(){
     println!("{}", name_reference)
 }
 
+fn slicing(){
+    let array = [1,2,3,4,5,6,7,8,9];
+    let slice = &array[1..5];
+    println!("{:?}", slice);
+}
+
 
 
 fn main() {
-
-    reference();
+    slicing();
+    // reference();
 
    let x = true;
         //   read(x);
